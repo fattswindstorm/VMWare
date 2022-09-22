a2468c62f25c37660bb61fef450e852f0e9c650e
Matthew Windham <windham.matt@gmail.com>
2022-09-22 17:50:10 -0500

    created wmare module and manifest

---
+++ vmware.psm1
@@ -0,0 +1,54 @@
+
+
+
+function New-EsxiHosts {
+    [CmdletBinding()]
+
+    param (
+        $VcenterServer = 'Vcenter2'
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        
+    
+    end {
+        
+    }
+}
+
+
+function New-EsxiHost {
+    [CmdletBinding()]
+    param (
+        
+    )
+        <#
+.SYNOPSIS
+Adds an new Esxi host to your vcenter server
+.DESCRIPTION
+Long description
+
+.EXAMPLE
+New-EsxiHost -ComputerName ExampleHost1,ExampleHost2 -Vcenter $Vcenter -EsxiUser Root -EsxiPassword 'Pa$$w0rd' -VcenterCredential $cred
+
+.NOTES
+General notes
+
+#>
+    
+    begin {
+        
+    }
+    
+    process {
+        
+    }
+    
+    end {
+        
+    }
+}
